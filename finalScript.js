9c4583f6c9cafdbefb1049641e5a88c259ced9b0
Aman Kumar <50510960+AmanChhing@users.noreply.github.com>
2019-11-24 18:47:24 +0530

    Update finalScript.js

--- finalScript.js
+++ finalScript.js
@@ -250,7 +250,7 @@ $.getJSON("https://api.serpwow.com/live/search?api_key=ED9FF3B028DB4F02A7CEB801B
 		document.getElementById("block").innerHTML = createFrame("Dictionary.jpg");
 		related_Questions(data);
 	}
-	if(data.inline_images)
+	/*if(data.inline_images)
 	{
 		document.getElementById("block").innerHTML = "<div class='slideshow-container'>"
 		for(var img in data.inline_images)
@@ -260,7 +260,7 @@ $.getJSON("https://api.serpwow.com/live/search?api_key=ED9FF3B028DB4F02A7CEB801B
 		document.getElementById("block").innerHTML += "<a class='prev' onclick='plusSlides(-1)'>&#10094;</a>"+"<a class='next' onclick='plusSlides(1)'>&#10095;</a></div><br>"
 
 		document.getElementById("block").innerHTML +='<div style="text-align:center"><span class="dot" onclick="currentSlide(1)"></span><span class="dot" onclick="currentSlide(2)"></span><span class="dot" onclick="currentSlide(3)"></span></div>'
-	} 
+	} */
 	else if(data.answer_box)
 	{
 		if(data.answer_box.answers)
@@ -676,33 +676,3 @@ $(document).ready(function(){
     document.getElementById('footer').scrollIntoView(false);
 });
 
-
-
-var slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-}
